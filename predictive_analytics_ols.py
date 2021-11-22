687814753021723d4ef55eb4c5d3e9e12452bb9e
Ralf Müller <ralf.mullerfernandez@ucdconnect.ie>
2021-11-22 14:03:32 +0000

    Update v1 visualization and practice GUI 2

--- predictive_analytics_ols.py
+++ predictive_analytics_ols.py
@@ -1,64 +1,58 @@
 # Importing all the relevant libraries for the OLS Regression
 import numpy as np
+import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression # We use the Linear Regression from Sklearn
 import seaborn as sns # We decided to import seaborn to update the graphs and plots from matplotlib
 from datetime import datetime
-import copy # Generate deepcopies of our dataset to run the model each time
+import stk_requestF as stkr
+import tkinter as tk
+import dataframe_image as dfi
+
 sns.set()
 
 ''' Generate a simple OLS regression model for one data set
     The basis for the OLS model was obtained through the knowledge acquired in the Udemy Course:
     The Data Science Course 2021: Complete Data Science Bootcamp'''
-def sk_simple_OLS(stocks, size):
+def sk_simple_OLS(stocks, company, t_size, size):
     # Generating the two Linear Models for the Test and Plots
-    inputs, targets, x_train, x_test, y_train, y_test, company, company2 = gen_model(stocks, size)
+    inputs, targets, x_train, x_test, y_train, y_test= gen_model(stocks, company, t_size, size)
     reg, predicted_v = model_OLS(inputs, x_train, y_train)
-    month = get_month(stocks, company)
+    month = get_month(stocks, company[0])
     
     # Generate the Plots and output from the linear regression model considering if we have 1 or two stocks
-    test_result = sk_test_OLS(x_test, y_test, reg)
     if size == 1:
-        plot_OLS1(inputs, targets, predicted_v, reg, company, month)
+        plot_OLS1(inputs, targets, predicted_v, reg, company[0], month)
     elif size == 2:
-        plot_OLS2(inputs, targets, predicted_v, reg, company, company2)
+        plot_OLS2(inputs, targets, predicted_v, reg, company[0], company[1])
     plot_yhat_v_y(x_train, y_train, reg)
     
     # Create the variables to Return to the user
-    model= sm.OLS(y_train,x_train).fit()
+    test_result = sk_test_OLS(x_test, y_test, reg)
+    test_styled = test_result.style.background_gradient()
+    dfi.export(test_styled,"testtable.png")
+    #model= sm.OLS(y_train,x_train).fit() # Overall analysis of the model
+    # Create the variables to Return to the user
     RSME = sm.tools.eval_measures.rmse(predicted_v, targets, axis=0)
     R2 = reg.score(x_train,y_train)
-    # print(model.summary()) # Show the user all the information regarding the data
-    # print(test_result) # Show the user how the model performed on the test data
     
     return RSME, R2, reg
 
-''' This will return the results table from the OLS
-    model, test_results, RSME = sk_simple_OLS(stock_dict2)
-    model.summary() # Get a summary from the OLS Model
-    print(test_results) # Get a table with the results from the model applied to Test Data'''
-
-
 # Generate the SKLearn OLS model to generate the graphs and obtain the results based on 1 or 2 stocks
-def gen_model(stocks, size):
+def gen_model(stocks, company, t_size, size):
     # Import library to split the data into test and training values
     from sklearn.model_selection import train_test_split
-    t_size = get_t_size()
-    while t_size > 1 or t_size <=0:
-        t_size = float(input("Please select the a valid number between 0 and 1: "))
-    company = get_company(stocks)
+    val_company(stocks,company)
+    t_size = val_t_size(t_size)
     if size == 1:
-        targets, inputs = get_price_date(stocks, company)
-        company2 = 0
+        targets, inputs = get_price_date(stocks, company[0])
     elif size == 2:
-        company2 = input('To which stock do you wish to compare '+ company + ': \n' + ', '.join(list(stocks))
- + '? ')
         targets = stocks[company]['5. adjusted close']
-        inputs = stocks[company2]['5. adjusted close']
+        inputs = stocks[company[1]]['5. adjusted close']
     inputs2 = sm.add_constant(inputs) # Transform the data to add the constant value
     x_train, x_test, y_train, y_test = train_test_split(inputs2, targets, test_size=t_size, random_state=365)
-    return inputs, targets, x_train, x_test, y_train, y_test, company, company2
+    return inputs, targets, x_train, x_test, y_train, y_test
 
 # Generate the Basic Statsmodels OLS to generate the graphs and obtain the results
 def model_OLS(inputs, x_train, y_train):
@@ -75,9 +69,9 @@ def plot_OLS1(inputs, targets, predicted_v, reg, company, month):
     plt.xlabel('Date', fontsize = 10)
     plt.ylabel('Price', fontsize = 10)
     plt.legend()
-    plt.show()
+    #plt.savefig('ols_regression.png')
+    #plt.show()
 
-    
 # Plotting the results from the regression model vs. the original scatterplot of stock1 vs. stock2
 def plot_OLS2(inputs, targets, predicted_v, reg, company, company2):
     plt.scatter(inputs,targets)
@@ -86,8 +80,9 @@ def plot_OLS2(inputs, targets, predicted_v, reg, company, company2):
     plt.xlabel(company2, fontsize = 10)
     plt.ylabel(company, fontsize = 10)     
     plt.legend()
-    plt.show()
-    
+    #plt.savefig('ols2stocks.png')
+    #plt.show()
+
 # Plot the values from the model vs. the real values
 def plot_yhat_v_y(x_train, y_train, reg):
     y_hat = reg.predict(x_train)
@@ -95,15 +90,14 @@ def plot_yhat_v_y(x_train, y_train, reg):
     plt.title('Expected Results vs. Real Results',fontsize=18)
     plt.xlabel('Real Values',fontsize=10)
     plt.ylabel('Model Predictions',fontsize=10)
-    plt.show() 
+    #plt.savefig('y_train_v_y_hat.png')
+    #plt.show() 
 
-# Get the company that the user will be working on while searching for the company in downloaded data
-def get_company(stocks):
-    company = input("For which company do you wish to make your analysis: \n" + ', '.join(list(stocks))
- + '? ')
-    while company not in list(stocks):
-        company = input("Please enter a valid stock that has been downloaded: \n" + ', '.join(list(stocks))+': ')
-    return company
+# Function to validate if Symbol is in the downloaded dictionary
+def val_company(stocks,companies):
+    for company in companies:
+        if company not in list(stocks):
+            raise tk.messagebox.showerror("Error", "Stock not found in downloaded data. Did you use the correct symbol? Please try again")
 
 #Function to obtain the date & prices for each graph
 def get_price_date(stocks, company):
@@ -117,12 +111,14 @@ def get_month(stocks, company):
     return month
 
 # Obtain the test size for the regression model
-def get_t_size():
+def val_t_size(t_size):
     while True:
         try:
-            t_size = float(input("Please select the Test Size as a decimal point: "))
+            t_size = float(t_size)
+            if t_size > 1 or t_size <=0:
+                raise tk.messagebox.showerror("Error", "Please enter a number between 0 and 1")
         except ValueError:
-            print('Your input is not a number')
+            raise tk.messagebox.showerror("Error", "Please enter a number for your Test Size")
             continue
         break
     return t_size
@@ -136,28 +132,26 @@ def sk_test_OLS(x_test, y_test, reg):
     test_table['Residual'] = test_table['Target'] - test_table['Prediction'] # Calculate the residual
     test_table['Square Error'] = test_table['Residual'] * test_table['Residual'] # Calculate Square Error
     test_table['Difference%'] = np.absolute(test_table['Residual']/test_table['Target']*100)
-    #pd.set_option('display.float_format', lambda x: '%.2f' % x)
+    pd.set_option('display.float_format', lambda x: '%.2f' % x)
     test_table.sort_values(by=['Difference%'])
     return test_table 
 
 
 # This function returns the predicted values and the graphs required for Predictive Analytics
-def sk_predval_OLS(stocks, size = 1):
-    RSME, R2, reg = sk_simple_OLS(stocks, size)
-    date = input("For which date do you wish to know the prediction? \nPlease use YYYY-MM-DD format: ")
-    validate(date)
+def sk_predval_OLS(stocks, companies, t_size, date, size = 1):
+    RSME, R2, reg = sk_simple_OLS(stocks, companies, t_size, size)
+    stkr.validate(date,date)
     coef = datetime.strptime(date, "%Y-%m-%d").timestamp()
     pred_test = reg.intercept_+reg.coef_[1]*coef
-    return pred_test, date, RSME, R2
-#pred_test, date, RSME, R2 = sk_predval_OLS(stock_dict2)
-#print("The predicted value for ", date, " is: ", round(pred_test,2))
-#print("The coefficient of determination of the model is: ", round(R2,2))
-#print("The RSME for the model is: ", round(RSME,2))
+    str_ols = "The predicted value for " + str(date) + " is: " + str(round(pred_test,2)) + \
+        "\nThe coefficient of determination of the model is: " + str(round(R2,2))+\
+        "\nThe RSME for the model is: " + str(round(RSME,2))    
+    return str_ols
+
 
 
 # This function returns the correlation between two stocks
-def OLS_two_stocks(stocks, size =2):
-    RSME, R2, reg = sk_simple_OLS(stocks, size)
-    print("The coefficient of correlation between stocks is: ", round(R2,2))
-    print("The RSME for the model is: ", round(RSME,2))
-#OLS_two_stocks(stock_dict2)
+def OLS_two_stocks(stocks, companies, t_size, size =2):
+    RSME, R2, reg = sk_simple_OLS(stocks, companies, t_size, size)
+    ols2_str = "The coefficient of correlation between stocks is: " + str(round(R2,2)) +"\nThe RSME for the model is: "+str(round(RSME,2))
+    return ols2_str
--- visualizations.py
+++ visualizations.py
@@ -1,21 +1,27 @@
+import matplotlib.pyplot as plt
+import predictive_analytics_ols as stkols
+import tkinter as tk
+import numpy as np
+import pandas as pd
+
 # Function to get the window for the data visualization
-def get_window():
+def val_window(win_size):
     while True:
         try:
-            win_size = abs(int(input('Please Choose a Window Size for your moving average: ')))
+            win_size = abs(int(win_size))
         except ValueError:
-            print('Your input is not a number')
+            raise tk.messagebox.showerror("Error", "Please enter a number for your Window Size")
             continue
         break
     return win_size
 
 # Get the Standard Deviation for the Bollinger Bands
-def get_std():
+def val_std(st_dev):
     while True:
         try:
-            num_std = abs(float(input('How many standard deviations away from the mean do you wish to calculate? ')))
+            num_std = abs(float(st_dev))
         except ValueError:
-            print('Your input is not a number')
+            raise tk.messagebox.showerror("Error", "Please enter a number for your Standard Deviation")
             continue
         break
     return num_std
@@ -24,110 +30,111 @@ def get_std():
 def get_ema(emas):
     while True:
         try:
-            ema = abs(float(input('Please enter the length of the {}: '.format(emas))))
+            ema = abs(float(emas))
         except ValueError:
-            print('Your input is not a number')
+            raise tk.messagebox.showerror("Error", "Please enter a number for your EMA and Signal Line")
             continue
         break
     return ema
 
 #Function to obtain data for the MACD
-def get_data_macd():
+def val_data_macd(fema, sema, smooth):
     still_ask = True
     while still_ask == True:
-        fema = get_ema("fast EMA")
-        sema = get_ema("slow EMA")
+        fema = get_ema(fema)
+        sema = get_ema(sema)
         if sema >= fema:
-            print('Please enter a fast EMA greater than the slow EMA')
+            raise tk.messagebox.showerror("Error",'Please enter a Fast EMA greater than the Slow EMA')
         else:
             still_ask = False
-    smooth = get_ema("the period of the Signal line")
+    smooth = get_ema(smooth)
     return fema, sema, smooth
 
 # Returns the Raw Time Series of the Selected Stock
-def raw_time_series(stocks): 
-    company = get_company(stocks)
-    price, date = get_price_date(stocks, company)
-    month = get_month(stocks, company)
+def raw_time_series(stocks, company): 
+    #company = get_company(stocks)
+    stkols.val_company(stocks, company)
+    price, date = stkols.get_price_date(stocks, company[0])
+    month = stkols.get_month(stocks, company[0])
     plt.plot(month, price)
-    plt.title('Raw Time Series '+ company,fontsize=18)
+    plt.title('Raw Time Series '+ company[0],fontsize=18)
     plt.xlabel('Date', fontsize = 10)
     plt.ylabel('Adj. Closing Price', fontsize = 10)
+    plt.savefig('Raw_ts.png')
     plt.show()
-#raw_time_series(stock_dict2)
 
 # Returns the trend line for the data series
-def plot_trend_line(stocks):
-    inputs, targets, x_train, x_test, y_train, y_test, company, company2 = gen_model(stocks, size=1)
-    month = get_month(stocks, company)
-    reg, predicted_v = model_OLS(inputs, x_train, y_train)
-    plot_OLS1(inputs, targets, predicted_v, reg, company, month)
-#plot_trend_line(stock_dict2)
+def plot_trend_line(stocks, company, t_size, size = 1):
+    stkols.val_company(stocks, company)    
+    inputs, targets, x_train, x_test, y_train, y_test = stkols.gen_model(stocks, company, t_size, size)
+    month = stkols.get_month(stocks, company[0])
+    reg, predicted_v = stkols.model_OLS(inputs, x_train, y_train)
+    stkols.plot_OLS1(inputs, targets, predicted_v, reg, company[0], month)
 
 # Plot Moving Averages for Stocks given a Window Time
-def moving_averages(stocks):
-    company = get_company(stocks)
-    win_size = get_window()
-    price, date = get_price_date(stocks, company)
+def moving_averages(stocks, company, win_size):
+    price, date = stkols.get_price_date(stocks, company[0])
     sma = price.rolling(window =win_size).mean()
     std = price.rolling(window =win_size).std()
-    return sma, std, company, win_size, price
+    return sma, std, price
 
-def plot_sma(stocks):
-    sma, std, company, win_size, price = moving_averages(stocks)
-    month = get_month(stocks, company)
-    plt.title('SMA '+ company,fontsize=18)
+def plot_sma(stocks, company, win_size):
+    stkols.val_company(stocks, company)
+    win_size = val_window(win_size)
+    sma, std, price = moving_averages(stocks, company, win_size)
+    month = stkols.get_month(stocks, company[0])
+    plt.title('SMA '+ company[0],fontsize=18)
     plt.xlabel('Date', fontsize = 10)
     plt.ylabel('Adj. Closing Price', fontsize = 10)
     plt.plot(month, price, label="Closing Prices")
     plt.plot(month, sma, label=str(win_size)+' Day SMA')
     plt.legend()
-#plot_sma(stock_dict2)
+    plt.savefig('moving_averages.png')
 
 # Plotting Bollinger Bands
-def bollinger_band(stocks):
-    sma, std, company, win_size, price = moving_averages(stocks)
-    num_std = get_std()
+def bollinger_band(stocks, company, win_size, stdev):
+    stkols.val_company(stocks, company)
+    win_size = val_window(win_size)
+    num_std = val_std(stdev)
+    sma, std, price = moving_averages(stocks, company, win_size)
     upper_b = sma + std * num_std
     lower_b = sma - std * num_std
-    return upper_b , lower_b, company
+    return upper_b , lower_b
 
-def plot_bollinger(stocks):
-    upper_b , lower_b, company = bollinger_band(stocks)
-    month = get_month(stocks, company)
-    plt.title('Bollinger Band '+ company,fontsize=18)
+def plot_bollinger(stocks, company, win_size, stdev):
+    upper_b , lower_b = bollinger_band(stocks, company, win_size, stdev)
+    month = stkols.get_month(stocks, company[0])
+    plt.title('Bollinger Band '+ company[0],fontsize=18)
     plt.xlabel('Date', fontsize = 10)
     plt.ylabel('Adj. Closing Price', fontsize = 10)
-    plt.plot(month, stocks[company]['5. adjusted close'], label="Closing Prices")
+    plt.plot(month, stocks[company[0]]['5. adjusted close'], label="Closing Prices")
     plt.plot(month, upper_b, label='Bollinger Up', c='g')
     plt.plot(month, lower_b, label='Bollinger Down', c='r')
     plt.legend()
-    plt.show()
-#plot_bollinger(stock_dict2)    
+    plt.savefig('bollinger_bands.png')
 
 # Plot Weighted Moving Averages from Data Set
-def wma(stocks):
-    company = get_company(stocks)
-    win_size = get_window()
-    price, date = get_price_date(stocks, company)
-    month = get_month(stocks, company)
+def plot_wma(stocks, company, win_size):
+    stkols.val_company(stocks, company)
+    win_size = val_window(win_size)
+    price, date = stkols.get_price_date(stocks, company[0])
+    month = stkols.get_month(stocks, company[0])
     weights = np.array([(i+1)/sum(range(win_size+1)) for i in range(win_size)])
-    plt.title('Weighted Moving Average '+ company,fontsize=18)
+    plt.title('Weighted Moving Average '+ company[0],fontsize=18)
     plt.xlabel('Date', fontsize = 10)
     plt.ylabel('Adj. Closing Price', fontsize = 10)
-    wma = stocks[company]['5. adjusted close'].rolling(window =win_size).apply(lambda x: np.sum(weights*x))
+    wma = stocks[company[0]]['5. adjusted close'].rolling(window =win_size).apply(lambda x: np.sum(weights*x))
     plt.plot(month, price, label="Closing Prices")
     plt.plot(month, wma, label=str(win_size)+' Day WMA')
     plt.legend()
-    plt.show()
-#wma(stock_dict2)
+    plt.savefig('wma.png')
 
 # Plot MACD of time series
 # The code for MACD adapted for the project was obtained from https://medium.com/codex/algorithmic-trading-with-macd-in-python-1c2769a6ad1b
-def obtain_macd(stocks):
-    company = get_company(stocks)
-    price, date = get_price_date(stocks, company)
-    fema, sema, smooth = get_data_macd()
+def obtain_macd(stocks, company, fema, sema, smooth):
+    stkols.val_company(stocks, company)
+    fema, sema, smooth = val_data_macd(fema, sema, smooth)
+    price, date = stkols.get_price_date(stocks, company[0])
     fast = price.ewm(span = fema, adjust = False).mean()
     slow = price.ewm(span = sema, adjust = False).mean()
     macd = pd.DataFrame(fast - slow).rename(columns = {'5. adjusted close':'macd'})
@@ -135,12 +142,12 @@ def obtain_macd(stocks):
     hist = pd.DataFrame(macd['macd'] - signal['signal']).rename(columns = {0:'hist'})
     frames =  [date, macd, signal, hist]
     macd_df = pd.concat(frames, join = 'inner', axis = 1)
-    return macd_df, company, price
+    return macd_df, price
 
-def plot_macd(stocks):
-    macd_df, company, price = obtain_macd(stocks)
+def plot_macd(stocks, company, fema, sema, smooth):
+    macd_df, price = obtain_macd(stocks, company, fema, sema, smooth)
     hist = macd_df['hist']
-    plt.title('MACD '+ company,fontsize=18)
+    plt.title('MACD '+ company[0],fontsize=18)
     ax1 = plt.subplot2grid((8,1), (0,0), rowspan = 5, colspan = 1)
     ax2 = plt.subplot2grid((8,1), (5,0), rowspan = 3, colspan = 1)
     plt.xlabel('Date', fontsize = 10)
@@ -155,12 +162,11 @@ def plot_macd(stocks):
             ax2.bar(price.index[i], hist[i], color = '#26a69a')
 
     plt.legend(loc = 'lower right')
-#plot_macd(stock_dict2)
+    plt.savefig('macd.png')
 
 # Plot Relative Strength Index for the selected Stock
 # The code was adapted to the program from https://tcoil.info/compute-rsi-for-stocks-with-python-relative-strength-index/
-def obtain_rsi(stocks, company, date, price):
-    win_size = get_window() 
+def obtain_rsi(stocks, company, win_size, date, price):
     change = price.diff(1).dropna() # The changes will be calculated on a daily basis
     gain = 0 * change
     gain[change > 0] = change[change > 0]
@@ -172,15 +178,16 @@ def obtain_rsi(stocks, company, date, price):
     RSI = 100 - (100/(1+abs(rsn)))
     return RSI
 
-def plot_rsi(stocks):
-    company = get_company(stocks)
-    price, date = get_price_date(stocks, company)
-    month = get_month(stocks, company)
-    stocks[company]['rsi'] = obtain_rsi(stocks, company, date, price)
-    plt.title('Relative Strength Index '+company,fontsize=18)
+def plot_rsi(stocks, company, win_size):
+    stkols.val_company(stocks, company)
+    win_size = val_window(win_size)
+    price, date = stkols.get_price_date(stocks, company[0])
+    month = stkols.get_month(stocks, company[0])
+    stocks[company[0]]['rsi'] = obtain_rsi(stocks, company, win_size, date, price)
+    plt.title('Relative Strength Index '+company[0],fontsize=18)
     plt.xlabel('Date', fontsize = 10)
     plt.ylabel('RSI', fontsize = 10)
-    plt.plot(month, stocks[company]['rsi'])
+    plt.plot(month, stocks[company[0]]['rsi'])
     plt.axhline(0, linestyle='--', alpha=0.1)
     plt.axhline(20, linestyle='--', alpha=0.5, color = 'red')
     plt.axhline(30, linestyle='--', color='grey')
@@ -188,14 +195,13 @@ def plot_rsi(stocks):
     plt.axhline(70, linestyle='--', color='grey')
     plt.axhline(80, linestyle='--', alpha=0.5, color = 'red')
     plt.axhline(100, linestyle='--', alpha=0.1)
-    plt.show()
-#plot_rsi(stock_dict2)
+    plt.savefig('rsi.png')
 
-def auto_correl(stocks):
-    company = get_company(stocks)
-    price, date = get_price_date(stocks, company)
+# Plot Autocorrelation from Stock
+def auto_correl(stocks, company):
+    stkols.val_company(stocks, company)
+    price, date = stkols.get_price_date(stocks, company[0])
     graph = pd.plotting.autocorrelation_plot(price)
-    plt.title('Autocorrelation Plot '+ company,fontsize=18)
+    plt.title('Autocorrelation Plot '+ company[0],fontsize=18)
     graph.plot()
-    plt.show()
-#auto_correl(stock_dict2)
+    plt.savefig('autocorrelation.png')
